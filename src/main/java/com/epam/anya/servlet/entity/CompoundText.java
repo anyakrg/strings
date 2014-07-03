c7672b696ec84f6b35a8f300763ce187d6a9cc0a
anyakrg <anya.tolstigina@mail.ru>
2014-07-03 10:41:32 +0600

    add log4j

--- src/main/java/com/epam/anya/servlet/entity/CompoundText.java
+++ src/main/java/com/epam/anya/servlet/entity/CompoundText.java
@@ -2,15 +2,15 @@ package com.epam.anya.servlet.entity;
 
 import java.util.List;
 
-public class CompoundText<T> {
-    private List<T> components;
-
-}
-
-class SText extends CompoundText<Paragraph>{
-
-}
-
-class Paragraph extends CompoundText<Sentence>{
-
-}
+//public class CompoundText<T> {
+//    private List<T> components;
+//
+//}
+//
+//class SText extends CompoundText<Paragraph>{
+//
+//}
+//
+//class Paragraph extends CompoundText<Sentence>{
+//
+//}
